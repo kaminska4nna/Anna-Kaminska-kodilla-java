805be1fcfcb71ab7991f58e331103627193b4573
kaminska4nna <kaminska4nna@users.noreply.github.com>
2017-10-12 09:50:29 +0200

    Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMain.java
@@ -1,4 +1,23 @@
 package com.kodilla.testing.calculator;
 
 public class TestingMain {
+    public static void main (String [] args){
+
+        Calculator calculator = new Calculator();
+        int additionResult = calculator.addition(3,3);
+        int substractionResult = calculator.subtraction(6,3);
+
+        if (additionResult == 6){
+            System.out.println("Adding is ok !");
+        }else {
+            System.out.println("Adding is not ok !");
+        }
+
+        if (substractionResult==3){
+            System.out.println("Substracting works fine !");
+        }else{
+            System.out.println("There is something wrong with substracting!");
+        }
+
+    }
 }
