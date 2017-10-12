c03eda1efce57819b040b8e9523d560a736f2481
kaminska4nna <kaminska4nna@users.noreply.github.com>
2017-10-12 09:51:32 +0200

    Calculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    public int addition(int x, int y)
+    {
+        return x + y;
+    }
+    public int subtraction(int x, int y)
+    {
+        return x - y;
+    }
+
+    }
+
