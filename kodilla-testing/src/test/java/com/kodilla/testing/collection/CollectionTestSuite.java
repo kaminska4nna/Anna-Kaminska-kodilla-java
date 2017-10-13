251ace51f091d312e21b5bfa22a9e90ab2819875
kaminska4nna <kaminska4nna@users.noreply.github.com>
2017-10-13 14:21:11 +0200

    Adding out dir to gitignore

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,6 +1,7 @@
 package com.kodilla.testing.collection;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,16 +27,22 @@ public class CollectionTestSuite {
     public void testOddNumbersExterminatorEmptyList() {
 
         System.out.println("Testing array is empty ");
-
         List<Integer> result = oddNumbersExterminator.exterminate(new ArrayList<>());
+        Assert.assertEquals(0, result.size());
 
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList() {
 
-        System.out.println("Testing ");
-
+        System.out.println("Testing if Even ArrayList is ok");
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+        arrayList.add(1);
+        arrayList.add(2);
+        List<Integer> expectedArrayList = new ArrayList<>();
+        expectedArrayList.add(2);
+        List<Integer> result = oddNumbersExterminator.exterminate(arrayList);
+        Assert.assertEquals(expectedArrayList, result);
     }
 
 }
