f9279eb12660eaec9df6c36d8680c9b23ba6d076
kaminska4nna <kaminska4nna@users.noreply.github.com>
2017-10-16 13:24:49 +0200

    Shape

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -3,32 +3,38 @@ package com.kodilla.testing.shape;
 import java.util.ArrayList;
 
 public class ShapeCollector {
+    Shape shape;
+
+    public ShapeCollector(Shape shape) {
+        this.shape = shape;
+    }
+
+    public Shape getShape() {
+        return shape;
+    }
+
     private ArrayList<Shape> figures;
 
 
-    public String addFigure(Shape shape) {
-        return null;
+    public void addFigure(Shape shape) {
+
+        figures.add(shape);
     }
 
-    ;
-
-    public String removeFigure(Shape shape) {
+    public boolean removeFigure(Shape shape) {
         figures.remove(shape);
-        return null;
+        return true;
     }
 
-    ;
-
-    public String getFigure(int n) {
-        return null;
+    public Shape getFigure(int n) {
+        return figures.get(n);
     }
 
-    ;
-
-    public String showFigures() {
-        return null;
+    public ArrayList<Shape> showFigures() {
+        return figures;
     }
 
-    ;
-
+    public int getFiguresQuantity() {
+        return figures.size();
+    }
 }
